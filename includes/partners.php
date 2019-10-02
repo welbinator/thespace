43c58069f77266c63fc8910f0999d2e704004305
james.welbes@gmail.com <james.welbes@gmail.com>
2019-10-01 22:52:39 -0500

    includes

---
+++ includes/partners.php
@@ -0,0 +1,56 @@
+ <div class="partner-area text-center pb-120">
+            <div class="container">
+                <div class="row justify-content-center align-items-center">
+                    <div class="col-md-6 col-md-offset-3 col-xs-12">
+                        <div class="section-title">
+                            <h2>our partners</h2>
+                        </div>
+                    </div>
+                </div>
+<!--   <div class="partner-owl owl-carousel"> 
+      <div class="single-partner">
+          <a href="#"><span><img loading="lazy" src="img/partner/allegra.jpg" alt="partner"></span></a>
+      </div>
+      <div class="single-partner">
+          <a href="#"><span><img loading="lazy" src="img/partner/imon.jpg" alt="partner"></span></a>
+      </div>
+      <div class="single-partner">
+          <a href="#"><span><img loading="lazy" src="img/partner/lacu.jpg" alt="partner"></span></a>
+      </div>
+      <div class="single-partner">
+          <a href="#"><span><img loading="lazy" src="img/partner/roasters.jpg" alt="partner"></span></a>
+      </div>
+      <div class="single-partner">
+          <a href="#"><span><img loading="lazy" src="img/partner/crisp.jpg" alt="partner"></span></a>
+      </div>
+      <div class="single-partner">
+          <a href="#"><span><img loading="lazy" src="img/partner/ross-street.jpg" alt="partner"></span></a>
+      </div>
+  </div>  -->
+              
+              
+            <div id="sponsorDiv">
+          <a href="https://www.allegramarketingprint.com/locations/cedar_rapids-ia/" target="_blank"><span><img loading="lazy" src="img/partner/allegra.jpg" alt="partner"></span></a>
+     
+<!--           <a href="https://www.imon.net/" target="_blank"><span><img loading="lazy" src="img/partner/imon.jpg" alt="partner"></span></a> -->
+      
+          <a href="https://www.linnareacu.org/" target="_blank"><span><img loading="lazy" src="img/partner/lacu.jpg" alt="partner"></span></a>
+      
+          <a href="https://www.facebook.com/Roasters-NewBo-656360344397781/" target="_blank"><span><img loading="lazy" src="img/partner/roasters.jpg" alt="partner"></span></a>
+     
+<!--           <a href="http://crispcr.com/" target="_blank"><span><img loading="lazy" src="img/partner/crisp.jpg" alt="partner"></span></a> -->
+     
+          <a href="https://www.rossstreetroasting.com/" target="_blank"><span><img loading="lazy" src="img/partner/ross-street.jpg" alt="partner"></span></a>
+              
+          <a href="https://www.officeevolution.com/locations/cedar-rapids" target="_blank"><span><img loading="lazy" src="img/partner/office-evolution.jpg" alt="partner"></span></a>
+              
+          <a href="https://aromaartisanpizza.com/" target="_bank"><span><img loading="lazy" src="img/partner/aroma.jpg" alt="partner"></span></a>
+              
+               <a href="https://iowabrewing.beer/" target="_bank"><span><img loading="lazy" src="img/partner/iowa-brewing.png" alt="partner"></span></a>
+    
+              </div>
+              
+              
+              
+            </div>
+        </div>
