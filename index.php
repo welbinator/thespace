65b900f5334edeb84358f95c68dae0d05190d7cb
James <james.welbes@gmail.com>
2020-03-24 21:01:06 -0500

    fixed email

--- index.php
+++ index.php
@@ -361,7 +361,7 @@
             <div class="row align-items-center">
                 <div class="col-12 col-md-6">
                     <h1 class="fancy-header mt-4">Contact us</h1>
-                    <p>208 12th ave SE Unit G, Cedar Rapids, IA 52401<br>Email : create@escapethecube.space<br>Phone : 319.360.5541</p>
+                    <p>208 12th ave SE Unit G, Cedar Rapids, IA 52401<br>Email : <a href="mailto:create@cedarrapidscoworking.space">create@cedarrapidscoworking.space</a><br>Phone : <a href="tel:3193605541">319.360.5541</a></p>
                 </div>
                 <div class="col-12 col-md-6"><img class="d-block ml-auto" src="assets/img/the_space_footer.png" width="150px"></div>
             </div>
