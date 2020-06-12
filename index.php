29e69356b0bacbd9ec9775a4ea5b787e0ce5d292
James Welbes <james.welbes@gmail.com>
2020-06-12 15:03:39 -0500

    added booking to menu

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
 </head>
 
 <body>
-    <nav class="navbar navbar-dark navbar-expand-md fixed-top">
+    <nav class="navbar navbar-dark navbar-expand-lg fixed-top">
         <div class="container-fluid"><a class="navbar-brand" href="#"><img class="logo" src="assets/img/logo.svg"></a><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
             <div class="collapse navbar-collapse" id="navcol-1">
                 <ul class="nav navbar-nav">
@@ -23,6 +23,7 @@
                     <li class="nav-item" role="presentation"><a class="nav-link" href="#pricing-js">Pricing</a></li>
                     <li class="nav-item" role="presentation"><a class="nav-link" href="#new-members-js">Members</a></li>
                     <li class="nav-item" role="presentation"><a class="nav-link" href="#contact">Contact</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" target="_blank" href="https://thespacecoworking.skedda.com/booking">Book a Studio</a></li>
                     <li class="nav-item" role="presentation"><a class="nav-link social" target="_blank" href="https://www.facebook.com/the.space.cr/"><i class="fab fa-facebook"></i><span>&nbsp;facebook</span></a></li>
                     <!-- <li class="nav-item" role="presentation"><a class="nav-link social mt-2 mt-md-0" href="#"><i class="fab fa-instagram"></i><span>&nbsp;Instagram</span></a></li> -->
                 </ul>
